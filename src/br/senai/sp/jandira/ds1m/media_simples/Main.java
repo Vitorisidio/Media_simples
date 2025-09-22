ee76b752de7fe4fdf525be27855b46f4f7394713
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2025-09-22 09:35:40 -0300

    Atividade de toma de desição utilizando if e else

---
+++ src/br/senai/sp/jandira/ds1m/media_simples/Main.java
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.ds1m.media_simples;
+
+import model.Calculadora;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando o Programa...");
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.obterDados();
+
+    }
+}
---
+++ src/model/Calculadora.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    String nomeAluno;
+    double nota1, nota2, nota3, nota4;
+    double resultadoDasNotas;
+
+    public void obterDados() {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("** CALCULADORA DE MÉDIA SIMPLES**");
+        System.out.println("----------------------------------");
+
+        System.out.print("Qual o nome do aluno? ");
+        nomeAluno = leitor.nextLine();
+
+        System.out.println("Qual é o a nota 1 do(a) aluno(a) " + nomeAluno + "?");
+        nota1 = leitor.nextDouble();
+
+        System.out.println("Qual é o a nota 2 do(a) aluno(a) " + nomeAluno + "?");
+        nota2 = leitor.nextDouble();
+
+        System.out.println("Qual é o a nota 3 do(a) aluno(a) " + nomeAluno + "?");
+        nota3 = leitor.nextDouble();
+
+        System.out.println("Qual é o a nota 4 do(a) aluno(a) " + nomeAluno + "?");
+        nota4 = leitor.nextDouble();
+
+        calcularMedia();
+    }
+
+    public void calcularMedia(){
+
+        resultadoDasNotas=(nota1+nota2+nota3+nota4) / 4;
+
+        resultado();
+
+    }
+
+        public void resultado(){
+
+            if (resultadoDasNotas >= 5) {
+
+                System.out.println( nomeAluno + " FOI APROVADO");
+
+            } else {
+
+                System.out.println(nomeAluno + " FOI REPROVADO");
+
+            }
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
